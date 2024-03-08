293baf580a6e40172954a20e84df37bb741452a4
ericpo1sh <ericpo1sh@gmail.com>
2024-03-08 11:40:22 -0600

    Lexical scoping and welcome message completed!

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,6 @@
+function welcome(firstName, lastName) {
+  function displayFullName(firstName, lastName) {
+    alert("Welcome " + firstName + " " + lastName + "!");
+  }
+  displayFullName("Holberton", "School");
+}
