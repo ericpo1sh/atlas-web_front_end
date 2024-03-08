a790021558c1fbcee612a46ae9369626a2451402
ericpo1sh <ericpo1sh@gmail.com>
2024-03-08 13:05:28 -0600

    Closure and loops task completed

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,16 @@
+function createClassRoom(numbersOfStudents) {
+  function studentSeat(seat) {
+    return function() {
+      return seat;
+    }
+  }
+  var students = [];
+
+  for (let i = 0; i <= numbersOfStudents; i++) {
+    var seatNumber = i + 1;
+    students.push(studentSeat(seatNumber));
+  }
+  return students;
+}
+
+var classRoom = createClassRoom(10);
